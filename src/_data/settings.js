a2029d3d5ecd9594cfa57c03789e58ecc6ae85d2
Shawn Thompson <plansmash@me.com>
2021-04-16 07:52:11 -0400

    update site settings

---
+++ src/_data/settings.js
@@ -0,0 +1,10 @@
+module.exports = {
+	buildTime: new Date(),
+	baseUrl: "/",
+	en: {
+	  metaTitle: "Digital Accessibility Toolkit"
+	},
+	fr: {
+	  metaTitle: "Boîte à outils d'accessibilité numérique"
+	}
+  };
