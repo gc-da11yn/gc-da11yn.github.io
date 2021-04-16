f14691b8fe4dc129e83b00c01cd41bc6b63c3f87
Shawn Thompson <plansmash@me.com>
2021-04-16 11:23:41 -0400

    locale heading include

---
+++ src/_data/header.js
@@ -0,0 +1,100 @@
+module.exports = {
+	fr: {
+		// Language bar
+		Languageselection: "Sélection de la langue",
+		Otherlanguage: "English",
+		Otherlanguagecode: "en",
+
+		// Brand
+		GovernmentofCanada: "Gouvernement du Canada",
+		GovernmentofCanadaURL: "https://www.canada.ca/fr.html",
+		SearchCanadaca: "Rechercher dans Canada.ca",
+		Search: "Recherche",
+
+		// Menu
+		MainMenu: "principal",
+		Jobsandtheworkplace: "Emplois et milieu de travail",
+		JobsandtheworkplaceURL: "https://www.canada.ca/en/services/jobs.html",
+		Immigrationandcitizenship: "Immigration et citoyenneté",
+		ImmigrationandcitizenshipURL: "https://www.canada.ca/en/services/immigration-citizenship.html",
+		Travelandtourism: "Voyage et tourisme",
+		TravelandtourismURL: "https://voyage.gc.ca/",
+		Businessandindustry: "Entreprises et industrie",
+		BusinessandindustryURL: "https://www.canada.ca/fr/services/entreprises.html",
+		Benefits: "Prestations",
+		BenefitsURL: "https://www.canada.ca/en/services/benefits.html",
+		Health: "Santé",
+		HealthURL: "https://www.canada.ca/fr/services/sante.html",
+		Taxes: "Impôts",
+		TaxesURL: "https://www.canada.ca/fr/services/impots.html",
+		Environmentandnaturalresources: "Environnement et ressources naturelles",
+		EnvironmentandnaturalresourcesURL: "https://www.canada.ca/fr/services/environnement.html",
+		Nationalsecurityanddefence: "Sécurité nationale et défense",
+		NationalsecurityanddefenceURL: "https://www.canada.ca/fr/services/defense.html",
+		Culturehistoryandsport: "Culture, histoire et sport",
+		CulturehistoryandsportURL: "https://www.canada.ca/fr/services/culture.html",
+		Policingjusticeandemergencies: "Services de police, justice et urgences",
+		PolicingjusticeandemergenciesURL: "https://www.canada.ca/fr/services/police.html",
+		Transportandinfrastructure: "Transport et infrastructure",
+		TransportandinfrastructureURL: "https://www.canada.ca/en/services/transport.html",
+		Canadaandtheworld: "Canada et le monde",
+		CanadaandtheworldURL: "http://international.gc.ca/world-monde/index.aspx?lang=fra",
+		Moneyandfinances: "Argent et finances",
+		MoneyandfinancesURL: "https://www.canada.ca/fr/services/finance.html",
+		Scienceandinnovation: "Science et innovation",
+		ScienceandinnovationURL: "https://www.canada.ca/fr/services/science.html",
+
+		// Breadcrumbs
+		Breadcrumbsheading: "Piste de navigation"
+	},
+	
+	en: {
+		// Language bar
+		Languageselection: "Language selection",
+		Otherlanguage: "Français",
+		Otherlanguagecode: "fr",
+
+		// Brand
+		GovernmentofCanada: "Government of Canada",
+		GovernmentofCanadaURL: "https://www.canada.ca/en.html",
+		SearchCanadaca: "Search Canada.ca",
+		Search: "Search",
+
+		// Menu
+		MainMenu: "Main",
+		Jobsandtheworkplace: "Jobs and the workplace",
+		JobsandtheworkplaceURL: "https://www.canada.ca/en/services/jobs.html",
+		Immigrationandcitizenship: "Immigration and citizenship",
+		ImmigrationandcitizenshipURL: "https://www.canada.ca/en/services/immigration-citizenship.html",
+		Travelandtourism: "Travel and tourism",
+		TravelandtourismURL: "https://travel.gc.ca/",
+		Businessandindustry: "Business and industry",
+		BusinessandindustryURL: "https://www.canada.ca/en/services/business.html",
+		Benefits: "Benefits",
+		BenefitsURL: "https://www.canada.ca/en/services/benefits.html",
+		Health: "Health",
+		HealthURL: "https://www.canada.ca/en/services/health.html",
+		Taxes: "Taxes",
+		TaxesURL: "https://www.canada.ca/en/services/taxes.html",
+		Environmentandnaturalresources: "Environment and natural resources",
+		EnvironmentandnaturalresourcesURL: "https://www.canada.ca/en/services/environment.html",
+		Nationalsecurityanddefence: "National security and defence",
+		NationalsecurityanddefenceURL: "https://www.canada.ca/en/services/defence.html",
+		Culturehistoryandsport: "Culture, history and sport",
+		CulturehistoryandsportURL: "https://www.canada.ca/en/services/culture.html",
+		Policingjusticeandemergencies: "Policing, justice and emergencies",
+		PolicingjusticeandemergenciesURL: "https://www.canada.ca/en/services/policing.html",
+		Transportandinfrastructure: "Transport and infrastructure",
+		TransportandinfrastructureURL: "https://www.canada.ca/en/services/transport.html",
+		Canadaandtheworld: "Canada and the world",
+		CanadaandtheworldURL: "https://international.gc.ca/world-monde/index.aspx?lang=eng",
+		Moneyandfinances: "Money and finances",
+		MoneyandfinancesURL: "https://www.canada.ca/en/services/finance.html",
+		Scienceandinnovation: "Science and innovation",
+		ScienceandinnovationURL: "https://www.canada.ca/en/services/science.html",
+		
+		// Breadcrumbs
+		Breadcrumbsheading: "Breadcrumb Navigation"
+
+	}
+};
