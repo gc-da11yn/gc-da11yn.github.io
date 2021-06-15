ac8f79f0b97b35036cd424c749592505520d93f0
Shawn Thompson <plansmash@me.com>
2021-06-15 14:08:39 -0400

    update underconstruction data value to be more detailed

--- src/_data/alerts.js
+++ src/_data/alerts.js
@@ -1,12 +1,12 @@
 module.exports = {
 	fr: {
 		// underconstruction alert
-		text: "Notre site, qui sera bientôt génial, est nouveau et toujours en construction. Veuillez ouvrir un <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">« <span lang=\"en\">issue</span> » sur notre projet Github</a> si vous voyez quelque chose qui doit être corrigé ou si vous avez des recommandations."
+		underConstText: "Notre site, qui sera bientôt génial, est nouveau et toujours en construction. Veuillez ouvrir un <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">« <span lang=\"en\">issue</span> » sur notre projet Github</a> si vous voyez quelque chose qui doit être corrigé ou si vous avez des recommandations."
 	},
 
 	en: {
 
 		// underconstruction alert
-		text: "Our soon to be awesome site is new and still under construction. Please open an <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">issue on our Github project</a> if you see anything that needs to be fixed or if you have any recommendations."
+		underConstText: "Our soon to be awesome site is new and still under construction. Please open an <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">issue on our Github project</a> if you see anything that needs to be fixed or if you have any recommendations."
 	}
 }
