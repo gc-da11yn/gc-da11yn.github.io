776ce4aef06aae01cec0b71e91a99331a84fd267
Shawn Thompson <plansmash@me.com>
2021-06-25 13:42:25 -0400

    create alert logic and frontmatter conditions

--- src/_data/alerts.js
+++ src/_data/alerts.js
@@ -1,12 +1,29 @@
 module.exports = {
 	fr: {
-		// underconstruction alert
-		underConstText: "Notre site, qui sera bientôt génial, est nouveau et toujours en construction. Veuillez ouvrir un <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">« <span lang=\"en\">issue</span> » sur notre projet Github</a> si vous voyez quelque chose qui doit être corrigé ou si vous avez des recommandations."
+		// Alert section heading
+		alertHeading: "Informations importantes",
+
+		// Under construction alert, appears on all pages of the site while site is ramping up on content
+		underConstText: "Notre site, qui sera bientôt génial, est nouveau et toujours en construction. Veuillez ouvrir un <a hreflang=\"en\" href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">« <span lang=\"en\">issue</span> » sur notre projet Github</a> si vous voyez quelque chose qui doit être corrigé ou si vous avez des recommandations.",
+
+		// Draft alert, appears on on all pages that have isDraft: true in the front matter
+		draftText: "Le contenu de ces pages est à l'état de projet.",
+
+		// Translation to come alert, appears on on all pages that have needsTranslation: true in the front matter
+		translationText: "This content is only available in French for the moment. We are currently working on a English translation of the content. Please stay tuned."
 	},
 
 	en: {
+		// Alert section heading
+		alertHeading: "Important information",
 
-		// underconstruction alert
-		underConstText: "Our soon to be awesome site is new and still under construction. Please open an <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">issue on our Github project</a> if you see anything that needs to be fixed or if you have any recommendations."
+		// Under construction alert, appears on all pages of the site while site is ramping up on content
+		underConstText: "Our soon to be awesome site is new and still under construction. Please open an <a href=\"https://github.com/gc-da11yn/gc-da11yn.github.io/issues\">issue on our Github project</a> if you see anything that needs to be fixed or if you have any recommendations.",
+
+		// Draft alert, appears on on all pages that have isDraft: true in the front matter
+		draftText: "The content on this pages in draft form.",
+
+		// Translation to come alert, appears on on all pages that have needsTranslation: true in the front matter, also removes the other language link
+		translationText: "Ce contenu n'est disponible qu'en anglais pour le moment. Nous travaillons actuellement à une traduction française du contenu. Veuillez rester à l'écoute."
 	}
 }
