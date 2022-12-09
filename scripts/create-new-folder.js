250b07e08e97e8785627ee83bf3f0b284b9b4a79
Lorenzo <61562227+Lorenzo767@users.noreply.github.com>
2022-12-09 15:07:13 -0500

    New functionality: create sub-directories
    add the directory after en or fr as a command line argument to make a sub-directory inside the specified directory

--- scripts/create-new-folder.js
+++ scripts/create-new-folder.js
@@ -3,18 +3,32 @@ const ps = require('prompt-sync');
 const path = require('path');
 
 const prompt = ps(); 
-
+/*TODO
+  Error handling and a help function.
+  What are breadcrumbs. Learn how to communicate what exactly and how this script fixes a problem. 
+*/
 process.chdir("src")
-console.log("The new working directory is " + process.cwd());
 
-const folderName = prompt("What is the name of your new folder?: ");
-const titleEN = prompt("What is the *english* title value of your page?: ");
-const descriptionEN = prompt("What is the *english* description value of your page?: ");
+var args = process.argv.slice(2);
 
+
+var folderName = prompt("What is the name of your new folder?: ");
+const titleEN = prompt("What is the ENGLISH title of your page?: ");
+const descriptionEN = prompt("What is the ENGLISH description of your page?: ");
+var relativeFolderPath;
+
+if(args.length == 0){
+  args[0] = "";
+  relativeFolderPath = path.sep + folderName;
+}else{
+  relativeFolderPath = path.sep + args[0] + path.sep + folderName;
+}
+
+console.log(relativeFolderPath.split(path.sep).join(path.sep + path.sep));
 var mdContentEN = `
 ---
 title: ${titleEN}
-layout: layouts/base.njk
+layout: layouts${path.sep}base.njk
 description: ${descriptionEN}
 ---
 
@@ -25,38 +39,34 @@ var jsonContentEN = `
 	"breadcrumbs": [
 		{
 			"title": "${titleEN}",
-			"link": "/en/${folderName}/"
+			"link": "${path.sep}${path.sep}en${relativeFolderPath.split(path.sep).join(path.sep + path.sep)}"
 		}
 	]
 }
 `
 
 try {
-  if (!fs.existsSync("." + path.sep + "en" + path.sep + folderName)) {
-        fs.mkdirSync("." + path.sep + "en" + path.sep + folderName);
+  console.log("." + path.sep + "en" + path.sep + relativeFolderPath)
+  if (!fs.existsSync("." + path.sep + "en" + path.sep + relativeFolderPath)) {
+        fs.mkdirSync("." + path.sep + "en" + path.sep + relativeFolderPath);
     }   
 } catch (err) {
   console.error(err);
 }
 
-fs.writeFile("." + path.sep + "en" + path.sep + folderName + path.sep  + "index.md" , mdContentEN, function(err, result) {
+fs.writeFile("." + path.sep + "en" + path.sep + relativeFolderPath + path.sep  + "index.md" , mdContentEN, function(err, result) {
     if(err) console.log('error', err);});
-fs.writeFile("." + path.sep + "en" + path.sep + folderName + path.sep + `${folderName}.json`, jsonContentEN, function(err, result) {
+fs.writeFile("." + path.sep + "en" + path.sep + relativeFolderPath + path.sep + `${folderName}.json`, jsonContentEN, function(err, result) {
     if(err) console.log('error', err);});
 
 
-
-//---------------------------------------------------------------------------------------------
-
-
-
-const titleFR = prompt("What is the *french* title value of your page?: ");
-const descriptionFR = prompt("What is the *french* description value of your page?: ");
+const titleFR = prompt("What is the FRENCH title of your page?: ");
+const descriptionFR = prompt("What is the FRENCH description of your page?: ");
 
 var mdContentFR = `
 ---
 title: ${titleFR}
-layout: layouts/base.njk
+layout: layouts${path.sep}base.njk
 description: ${descriptionFR}
 ---
 
@@ -67,21 +77,21 @@ var jsonContentFR = `
 	"breadcrumbs": [
 		{
 			"title": "${titleFR}",
-			"link": "/en/${folderName}/"
+			"link": "${path.sep}${path.sep}fr${relativeFolderPath.split(path.sep).join(path.sep + path.sep)}"
 		}
 	]
 }
 `
 
 try {
-    if (!fs.existsSync("." + path.sep + "fr" + path.sep + folderName)) {
-          fs.mkdirSync("." + path.sep + "fr" + path.sep + folderName);
+    if (!fs.existsSync("." + path.sep + "fr" + path.sep + relativeFolderPath)) {
+          fs.mkdirSync("." + path.sep + "fr" + path.sep + relativeFolderPath);
       }
   } catch (err) {
     console.error(err);
   }
 
-  fs.writeFile("." + path.sep + "fr" + path.sep + folderName + path.sep + "index.md", mdContentFR, function(err, result) {
+  fs.writeFile("." + path.sep + "fr" + path.sep + relativeFolderPath + path.sep + "index.md", mdContentFR, function(err, result) {
     if(err) console.log('error', err);});
-fs.writeFile("." + path.sep + "fr" + path.sep + folderName + path.sep + `${folderName}.json`, jsonContentFR, function(err, result) {
+fs.writeFile("." + path.sep + "fr" + path.sep + relativeFolderPath + path.sep + `${folderName}.json`, jsonContentFR, function(err, result) {
     if(err) console.log('error', err);});
