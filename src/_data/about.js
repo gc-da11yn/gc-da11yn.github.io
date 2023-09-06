61eb07fcf75d6641f3379f56acdaf9210daff3b0
Shawn Thompson <plansmash@me.com>
2023-09-06 15:44:34 -0400

    homepage changes (#338)
    * Create collections for homepages
    
    * change banner style to include description
    move description in base layout
    create home layout
    create home data file
    create about partial (needs content)
    create about data file

---
+++ src/_data/about.js
@@ -0,0 +1,9 @@
+module.exports = {
+	fr: {
+		about: "À propos la Boîte à outils sur l'accessibilité numérique",
+	},
+
+	en: {
+		about: "About the Digital Accessibility Toolkit",
+	},
+};
---
+++ src/_data/home.js
@@ -0,0 +1,10 @@
+module.exports = {
+	fr: {
+		explore: "Découvrez votre boîte à outils",
+	},
+
+	en: {
+		explore: "Exploring your toolkit",
+	},
+};
+
