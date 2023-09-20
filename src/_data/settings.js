72256b973887bd9bc4b84acc7205ae581b4f2392
Shawn Thompson <plansmash@me.com>
2023-09-20 12:11:42 -0400

    create sitemap.xml add EleventyHtmlBasePlugin plugin create sitemap.njk file

--- src/_data/settings.js
+++ src/_data/settings.js
@@ -2,10 +2,12 @@ module.exports = {
 	buildTime: new Date(),
 	env: process.env.ELEVENTY_ENV,
 	baseUrl: "/",
+	url: "https://a11y.canada.ca/",
 	en: {
-	  metaTitle: "Digital Accessibility Toolkit / Sharing space"
+		metaTitle: "Digital Accessibility Toolkit / Sharing space",
 	},
 	fr: {
-	  metaTitle: "Boîte à outils d'accessibilité numérique / Espace de partage"
-	}
+		metaTitle:
+			"Boîte à outils d'accessibilité numérique / Espace de partage",
+	},
 };
