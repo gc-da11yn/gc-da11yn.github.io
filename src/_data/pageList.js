196f48a04b483072ba2c5398a4f22cb4bf99496f
Shawn Thompson <plansmash@me.com>
2023-10-12 09:09:53 -0400

    update to page list (#376)
    add filter option
    add link to file on github

---
+++ src/_data/pageList.js
@@ -0,0 +1,21 @@
+module.exports = {
+	fr: {
+		definitionButtonText: "Définitions des étiquettes",
+		filterButtonText: "Filtrer les pages",
+		subjectsText: "Sujets",
+		tagsText: "Balises",
+		descriptionText: "Description",
+		pathText: "Chemin d'accès",
+		filePathText: "Emplacement du fichier",
+	},
+
+	en: {
+		definitionButtonText: "Definitions of labels",
+		filterButtonText: "Filter pages",
+		subjectsText: "Subjects",
+		tagsText: "Tags",
+		descriptionText: "Description",
+		pathText: "Path",
+		filePathText: "File location",
+	},
+};
