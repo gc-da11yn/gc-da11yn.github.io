ad0475a6fa3a5e2c1eba1891ac788960d421229a
Isabel <isabelacevedo2000@hotmail.com>
2023-11-10 14:16:43 -0500

    link replacement added on build

--- .eleventy.js
+++ .eleventy.js
@@ -9,46 +9,17 @@ const path = require('path');
 module.exports = function (eleventyConfig) {
   let markdownItOptions = {
     html: true, // you can include HTML tags
-  };
+  };a
 
-  // Replace "./" with "../" and ".md" with "/"
   const customMarkdownIt = markdownIt(markdownItOptions)
     .use(markdownItAnchor)
-    .use(markdownItAttrs)
-    .use((md) => {
-      md.core.ruler.push('replace-links', (state) => {
-        state.tokens.forEach((token) => {
-          if (token.type === 'inline' && token.children) {
-            token.children.forEach((child) => {
-              if (child.type === 'link_open' && child.attrs) {
-                child.attrs.forEach((attr) => {
-                  if (attr[0] === 'href' && (attr[1].startsWith('./') || attr[1].startsWith('../'))) {
-                    // Replace './' with '../' and '.md' with '/'
-                    attr[1] = path.join('..', attr[1].replace(/^\.\//, '').replace(/\.md$/, '/'));
-                  }
-                });
-              } else if (child.type === 'link_open' && child.content) {
-                // Handle inline links like [text](./path/to/file.md)
-                const match = child.content.match(/\]\((\.{1,2}\/.*?\.md)\)/);
-                if (match) {
-                  const replacedLink = match[1].replace(/^\.\//, '').replace(/\.md$/, '/');
-                  child.content = child.content.replace(match[1], replacedLink);
-                }
-              }
-            });
-          }
-        });
-      });
-    });
+    .use(markdownItAttrs);
 
-  eleventyConfig.setLibrary(
-    "md",
-    customMarkdownIt
-  );
+  eleventyConfig.setLibrary('md', customMarkdownIt);
 
   const slugifyFilter = eleventyConfig.javascriptFunctions.slugify;
 
-  eleventyConfig.addFilter("stripTagsSlugify", (str) => {
+  eleventyConfig.addFilter('stripTagsSlugify', (str) => {
     if (!str) return;
     return slugifyFilter(stripHtml(str).result, {});
   });
@@ -68,20 +39,23 @@ module.exports = function (eleventyConfig) {
       .toFormat('yyyy-MM-dd');
   });
 
-  eleventyConfig.addPassthroughCopy({ "./src/_docs" : "docs" });
-  eleventyConfig.addPassthroughCopy({ "./src/_images" : "img" });
-  eleventyConfig.addPassthroughCopy({ "./src/CNAME" : "CNAME" });
+  eleventyConfig.addPassthroughCopy({ './src/_docs': 'docs' });
+  eleventyConfig.addPassthroughCopy({ './src/_images': 'img' });
+  eleventyConfig.addPassthroughCopy({ './src/CNAME': 'CNAME' });
+
+  // Run the replaceLinks.js script before generating the site
+  eleventyConfig.addJavaScriptFunction('replaceLinks', require('./scripts/replace-links'));
 
   return {
     dir: {
-      input : "src",
-      output : "_site",
-      includes : "_includes",
-      data : "_data"
+      input: 'src',
+      output: '_site',
+      includes: '_includes',
+      data: '_data',
     },
-    templateFormats : ["html", "md", "njk", "css"],
-    htmlTemplateEngine : "njk",
-    markdownTemplate : "njk",
-    setUseGitIgnore : false
+    templateFormats: ['html', 'md', 'njk', 'css'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplate: 'njk',
+    setUseGitIgnore: false,
   };
 };
