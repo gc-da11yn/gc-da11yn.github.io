22b1a9342d147af69d9bf466125be05b90425faa
Isabel <isabelacevedo2000@hotmail.com>
2023-11-10 14:16:43 -0500

    link replacement added on build

--- scripts/replace-links.js
+++ scripts/replace-links.js
@@ -1,18 +1,34 @@
-const markdownIt = require("markdown-it");
-const replaceLink = require("markdown-it-replace-link");
+const fs = require('fs');
+const path = require('path');
 
-const customMarkdownIt = markdownIt({
-  html: true,
-}).use(replaceLink, {
-  replaceLink: (link) => {
-    if (link.startsWith("./") || link.startsWith("../")) {
-      // Replace './' with '../' and '.md' with '/'
-      const newLink = link.replace(/^\.\//, "../").replace(/\.md$/, "/");
-      return newLink;
+function replaceLinksInFile(filePath) {
+  const content = fs.readFileSync(filePath, 'utf8');
+  const updatedContent = content.replace(/(<a[^>]+href=["'])(\.\.\/|\.{0,2}\/)(.*?\.md["'][^>]*>)/g, (match, p1, p2, p3) => {
+    return `${p1}../${p3.replace(/\.md(["'])/g, '/$1')}`;
+  });
+
+  fs.writeFileSync(filePath, updatedContent, 'utf8');
+}
+
+function processDirectory(directory) {
+  const files = fs.readdirSync(directory);
+
+  files.forEach((file) => {
+    const filePath = path.resolve(directory, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      processDirectory(filePath);
+    } else if (filePath.endsWith('.html') && fs.statSync(filePath).isFile()) {
+      replaceLinksInFile(filePath);
+      console.log(`Links replaced in ${filePath}`);
     }
-    return link;
-  },
-});
+  });
+}
 
+// Directory containing HTML files (assuming the _site directory)
+const siteDirectory = path.resolve(__dirname, '..', '_site');
 
-module.exports = customMarkdownIt;
+// Recursively process HTML files in subdirectories
+processDirectory(siteDirectory);
+
+console.log('Link replacement complete.');
