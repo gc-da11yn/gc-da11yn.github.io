08ef3f3727e666d3323f92aa18f40cabb3117bd9
Isabel <isabelacevedo2000@hotmail.com>
2023-11-30 13:17:33 -0500

    git fetching

---
+++ src/src.11tydata.js
@@ -0,0 +1,46 @@
+const spawn = require("cross-spawn");
+
+// https://github.com/11ty/eleventy/blob/main/src/Util/DateGitLastUpdated.js
+function getGitLastUpdatedTimeStamp(filePath) {
+	return (
+		parseInt(
+			spawn
+				.sync(
+					"git",
+					// Formats https://www.git-scm.com/docs/git-log#_pretty_formats
+					// %at author date, UNIX timestamp
+					["log", "-1", "--format=%at", filePath]
+				)
+				.stdout.toString("utf-8")
+		) * 1000
+	);
+}
+
+// https://github.com/11ty/eleventy/blob/main/src/Util/DateGitFirstAdded.js
+function getGitFirstAddedTimeStamp(filePath) {
+	return (
+		parseInt(
+			spawn
+				.sync(
+					"git",
+					// Formats https://www.git-scm.com/docs/git-log#_pretty_formats
+					// %at author date, UNIX timestamp
+					["log", "--diff-filter=A", "--follow", "-1", "--format=%at", filePath]
+				)
+				.stdout.toString("utf-8")
+		) * 1000
+	);
+}
+
+module.exports = {
+	eleventyComputed: {
+		modified: (data) => {
+			let timestamp = getGitLastUpdatedTimeStamp(data.page.inputPath);
+			return timestamp ? new Date(timestamp) : new Date();
+		},
+		created: (data) => {
+			let timestamp = getGitFirstAddedTimeStamp(data.page.inputPath);
+			return timestamp ? new Date(timestamp) : new Date();
+		},
+	},
+};
