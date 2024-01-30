788e0d8b1ddcc241bc1c9ff8753d23fa94580522
DESKTOP-5O5JLC2\Dhruv Patel <dhruvpatel250203@gmail.com>
2024-01-30 11:28:48 -0500

    made the landing page for tipsheets in FR and EN

--- src/_data/tagList.js
+++ src/_data/tagList.js
@@ -20,6 +20,7 @@ module.exports = {
 			contactUs: "Contactez-nous",
 			accessibilityInYourRole: "L'accessibilité dans votre rôle",
 			mainAbout: "Section à propos de nous sur la page d'accueil",
+			updatesMain: "mise à jour sur le principal",
 			office2016:
 				"Comment créer des documents accessibles dans Office 2016",
 			microsoft365:
@@ -46,6 +47,7 @@ module.exports = {
 			accessibilityInYourRole: "Accessibility in your role",
 			mainAbout: "About us section on homepage",
 			office2016: "How to create accessible documents in Office 2016",
+			updatesMain: "update on main",
 			microsoft365: "How to create accessible documents in Microsoft 365",
 		},
 	},
